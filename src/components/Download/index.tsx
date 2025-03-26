9d4f0340085d21ed08985820a44273f29e40e0e3
ilya <mmampr@mail.ru>
2025-03-26 19:14:34 +0300

    ignore

--- src/components/Download/index.tsx
+++ src/components/Download/index.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import s from "./Download.module.scss";
 
 const Download = () => {
-  return <div className={s.container}></div>;
+  return (
+    <div className={s.container}>
+      <div className={s.title}></div>
+      <div className={s.files}></div>
+    </div>
+  );
 };
 
 export { Download };
