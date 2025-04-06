cef8c0194bdc1c61add4b2b1a83fe606a8baf162
ilya <mmampr@mail.ru>
2025-04-06 08:26:35 +0300

    yup

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -6,33 +6,52 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 const schema = yup.object({
+  phone: yup
+    .string()
+    .required("Введите номер")
+    .min(10, "Минимум 11 цифр")
+    .max(11, "Максимум 11 цифр")
+    .matches(/([0-9]{3})[0-9]{3}-[0-9]{4}/, "Только цифры"),
   name: yup
     .string()
-    .required("ВВедите имя")
+    .required("Введите имя")
     .min(2, "Минимум 2 буквы")
     .max(10, "Максимум 10 букв")
     .matches(/^[a-zA-Zа-яА-Я]*$/, "Только буквы"),
+  organization: yup
+    .string()
+    .required("Введите название организации")
+    .min(2, "Минимум 2 буквы")
+    .matches(/^[a-zA-Zа-яА-Я]*$/, "Только буквы"),
+  mail: yup
+    .string()
+    .required("Введите e-mail")
+    .matches(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/),
 });
 
 const LABELS = [
-  { title: "Ваше имя", id: "name", meaning: "name" },
+  { title: "Ваше имя", id: "name", type: "text" },
   {
     title: "Название организации",
     id: "organization",
-    meaning: "organization",
+    type: "text",
   },
-  { title: "Телефон", id: "phone", meaning: "phone" },
-  { title: "E-mail", id: "mail", meaning: "mail" },
+  { title: "Телефон", id: "phone", type: "number" },
+  { title: "E-mail", id: "mail", type: "text" },
 ];
 
 interface LabelsType {
   title: string;
   id: string;
-  meaning: string;
+  type: string;
 }
 
 const Form = () => {
-  const { register, handleSubmit } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     mode: "onChange",
     resolver: yupResolver(schema),
   });
@@ -42,17 +61,18 @@ const Form = () => {
     <div className={s.container}>
       <h2 className={s.title}>Офис</h2>
       <form onSubmit={handleSubmit(onSubmit)} className={s.flex}>
-        {LABELS.map(({ title, id, meaning }: LabelsType, index) => (
+        {LABELS.map(({ title, id, type }: LabelsType, index) => (
           <div className={s.miniFlex} key={index}>
             <label htmlFor={id} className={s.label}>
               {title}
             </label>
             <input
-              {...register("name")} //как сюда вместо "name" поставить meaning?
+              {...register("mail")} //как сюда вместо "name" поставить meaning?
               id={id}
               className={s.input}
-              type="text"
-            ></input>
+              type={type}
+            />
+            <p className={s.text}>{errors.mail?.message}</p>
           </div>
         ))}
         <div className={s.area}>
